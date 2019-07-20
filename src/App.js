f9a71390bd8c17fd03e9b88632584ca46e6ef91b
Taylor Rogers <taylor.rogers71@gmail.com>
2019-07-20 11:59:54 -0700

    Added a bit of styling and content wrapper

--- src/App.js
+++ src/App.js
@@ -7,6 +7,20 @@ import Table from './components/organisms/Table';
 const ErrorMessage = styled.div`
   color: red;
   font-size: 0.8rem;
+  margin-botton: 0 0 2rem 1rem;
+`;
+
+const StyledInput = styled.input`
+  width: 10rem;
+  border-radius: 3px;
+  margin: 2rem 2rem 0 1rem;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
 function App() {
@@ -14,6 +28,9 @@ function App() {
   const [inputValue, setInputValue] = useState('');
   const [isLoading, setLoading] = useState(false);
   const [primeNumbers, setPrimeNumbers] = useState(null);
+  useEffect(() => {
+    document.body.style.backgroundColor = '#262627';
+  });
   useEffect(() => {
     let result;
     try {
@@ -29,15 +46,15 @@ function App() {
     }
   }, [inputValue]);
   return (
-    <>
-      <input
+    <Content>
+      <StyledInput
         onChange={event => setInputValue(event.target.value)}
         placeholder="Enter an integer"
         max="20000"
       />
       {error && <ErrorMessage>{error}</ErrorMessage>}
       {primeNumbers && <Table primeNumbers={primeNumbers} />}
-    </>
+    </Content>
   );
 }
 
