511022e0ff0f27e1560348fd785e945ea6f7f6c6
Taylor Rogers <taylor.rogers71@gmail.com>
2019-07-20 08:34:30 -0700

    Change App component to use table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import findPrimes from './helpers/findPrimes';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Table from './components/table';
+
+const ErrorMessage = styled.div`
+  color: red;
+  font-size: 0.8rem;
+`;
+
+class App extends React.Component {
+  state = {
+    error: '',
+    inputVal: '',
+    isLoading: false,
+    primeNumbers: null
+  };
+  async componentDidUpdate(prevProps, prevState) {
+    console.log(this.state, prevState);
+    let result;
+    if (this.state.inputVal !== prevState.inputVal) {
+      try {
+        if (this.state.inputVal !== '') {
+          this.setState({ error: '', isLoading: true });
+          result = await findPrimes(this.state.inputVal);
+        }
+        this.setState({ primeNumbers: result });
+      } catch (error) {
+        this.setState({ error: error.message });
+        return;
+      }
+    }
+    console.log(result);
+  }
+  render() {
+    console.log('app state', this.state);
+    const { primeNumbers } = this.state;
+    return (
+      <>
+        <input
+          onChange={event => this.setState({ inputVal: event.target.value })}
+          placeholder="Enter an integer"
+          max="20000"
+        />
+        {this.state.error && <ErrorMessage>{this.state.error}</ErrorMessage>}
+        {primeNumbers && <Table primeNumbers={primeNumbers} />}
+      </>
+    );
+  }
 }
 
 export default App;
